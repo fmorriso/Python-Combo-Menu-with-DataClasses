f4fd5866bd1864e2f8da58537d57f3f13b36c201
Frederick Morrison <frederick.morrison@outlook.com>
2024-03-01 14:42:44 -0700

    initial skeleton code

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def get_python_version() -> str:
+    return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
+
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        print(f'Combo Menu using Data Classes using python version {get_python_version()}')
