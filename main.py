7a3fc94718e25bfa2ce1d0d4bdecd1fe483d497e
Frederick Morrison <frederick.morrison@outlook.com>
2024-03-02 10:46:05 -0700

    move get_fries to SingleOrder

--- main.py
+++ main.py
@@ -36,59 +36,12 @@ def new_order() -> SingleOrder:
     return new_order
 
 
-def get_fries(order: SingleOrder) -> None:
-    if not get_yes_no_answer("Would you like fries?>"):
-        return
-
-    # filter the menu for just beverage choices
-    filtered_items = filter(lambda item: item.category == 'Fries', menu)
-    available_choices = list(filtered_items)
-
-    # create prompt
-    prompt = "What size french fries would you like to order: ("
-    for available_choice in available_choices:
-        prompt += f'{available_choice.name}: ${available_choice.price:.2f}, '
-    prompt = prompt.removesuffix(', ')
-    prompt += ") ?>"
-
-    # print(f'DEBUG: {prompt=}')
-    while True:
-        choice = input(prompt)
-        if choice is None or len(choice) == 0:
-            choice = "unknown"
-        abbrev = choice[:1].lower()
-
-        selection = None
-        for available_choice in available_choices:
-            if available_choice.name[:1].lower() == abbrev:
-                selection = available_choice
-                break
-
-        if selection is None:
-            print(f'{abbrev} is not a valid choice')
-        else:
-            print(f'{selection.name} is a great choice')
-            break
-
-    # Ask customer of they want to super-size their fries from Small to Large
-    if selection.name[:1].lower() == 's':
-        if get_yes_no_answer('Do you want to super-size to a large?>'):
-            abbrev = 'l'
-            for available_choice in available_choices:
-                if available_choice.name[:1].lower() == abbrev:
-                    selection = available_choice
-
-    order.fries_size = selection.name
-    order.fries_cost = selection.price
-
-    order.total_cost += selection.price
-
 
 def get_order():
     order = new_order()
     order.get_sandwich(menu)
     order.get_beverage(menu)
-    get_fries(order)
+    order.get_fries(menu)
     order.display()
 
 
