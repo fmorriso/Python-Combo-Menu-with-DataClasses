3ab855562faa47a27f9db2cfe7231d96667d3421
Frederick Morrison <frederick.morrison@outlook.com>
2024-03-14 11:19:37 -0600

    Change project to use GitLab instead of GitHub

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def get_order() -> SingleOrder:
 
 
 def get_python_version() -> str:
+    """returns the current version of python that is running the current program"""
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     print(f'Combo Menu using Data Classes using python version {get_python_version()}')
 
     restaurant = Restaurant("Fred's Fast Food")
-    print(f'Welcome to {restaurant.name} where the food is always delicious.')
+    print(f'Welcome to {restaurant.name} where the food and drinks are always delicious.')
     while True:
         current_order: SingleOrder = get_order()
         restaurant.add_order(current_order)
